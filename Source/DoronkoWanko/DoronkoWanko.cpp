63484a1ceeb9253d969853c43e46607b9bf09b02
Spacialist <a01050254473@gmail.com>
2024-07-24 14:31:39 +0900

    Setup

---
+++ Source/DoronkoWanko/DoronkoWanko.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DoronkoWanko.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DoronkoWanko, "DoronkoWanko" );
