35bd63579ce9ab9ff85879129ca40b08898b27d7
Spacialist <a01050254473@gmail.com>
2024-07-24 22:01:55 +0900

    Overlap_RotateWing

--- Source/DoronkoWanko/Private/HJ_ElectricFan.cpp
+++ Source/DoronkoWanko/Private/HJ_ElectricFan.cpp
@@ -4,6 +4,7 @@
 #include "HJ_ElectricFan.h"
 #include "GW_Player.h"
 #include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AHJ_ElectricFan::AHJ_ElectricFan()
@@ -11,16 +12,27 @@ AHJ_ElectricFan::AHJ_ElectricFan()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// 충돌체 생성 
 	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
 	SetRootComponent(BoxComp);
 	BoxComp->SetBoxExtent(FVector(50));
-
+	// 메쉬 생성 
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
 	MeshComp->SetupAttachment(RootComponent);
 
+	// 충돌체 처리 
 	BoxComp->SetCollisionProfileName(TEXT("MapObject"));
 	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &AHJ_ElectricFan::OnOverlapBegin);
 	BoxComp->OnComponentEndOverlap.AddDynamic(this, &AHJ_ElectricFan::OnOverlapEnd);
+
+	// 날개 메쉬 생성 
+	WingComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WingComp"));
+	WingComp->SetupAttachment(RootComponent);
+	// 회전 기본값 지정 
+	RotationSpeed = 200.0f;
+	CurrentRotationAngel = 0.0f;
+
+	IsInRange = false;
 
 }
 
@@ -36,14 +48,44 @@ void AHJ_ElectricFan::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// 범위 안에 들어왔을 때 OnAction 함수를 호출 
+	if (IsInRange)
+	{
+		APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+		if (PlayerController && PlayerController->IsInputKeyDown(EKeys::E))
+		{
+			RotationSpeed = 100.0f;
+			WingRota = WingRota + 1;
+		}
+	}
+	
+	if (WingRota % 2 == 1)
+	{
+		// 선풍기 날개 돌아가게 한다
+		// E키를 한번 누르면 돌아가고 & 두번 누르면 돌아가는게 멈춘다 
+		CurrentRotationAngel += RotationSpeed * DeltaTime;
+		if (CurrentRotationAngel > 360.0f)
+		{
+			CurrentRotationAngel -= 360.0f;
+		}
+
+		FRotator WingRotation = FRotator(0.0f, CurrentRotationAngel, 0.0f);
+		WingComp->SetRelativeRotation(WingRotation);
+
+		//충돌체를 소환한다 (물감과 부딪히면 특정 위치로 물감을 보내버린다) 
+		// else 일 때, 충돌체를 파괴한다 (내일 구현) 
+	}
+
 }
 
 void AHJ_ElectricFan::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// 플레이어와 충돌 인식 
 	auto* Player = Cast<AGW_Player>(OtherActor);
 
 	if (Player)
 	{
+		IsInRange = true;
 		UE_LOG(LogTemp, Warning, TEXT("CHECK"));
 	}
 
@@ -51,19 +93,12 @@ void AHJ_ElectricFan::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, A
 
 void AHJ_ElectricFan::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
+	// 플레이어와 충돌 종료 인식 
 	auto* Player = Cast<AGW_Player>(OtherActor);
 
 	if (Player)
 	{
+		IsInRange = false;
 		UE_LOG(LogTemp, Warning, TEXT("CHECKOUT"));
 	}
 }
-
-	// 플레이어가 가까이 오면 인식한다 (공통함수) 
-	// 조건을 2가지로 나눈다. A와 B 
-	// 
-// A일 때, 선풍기 날개 돌아간다. 
-// A일 때, 투명한 충돌체 소환한다 (선풍기 바람범위) // 충돌체에 따른 물감 이동은 물감 객체에서 구현 
-
-// B일 때, 선풍기 날개 멈춘다. 
-// B일 때, 투명한 충돌체 사라진다. 
--- Source/DoronkoWanko/Public/HJ_ElectricFan.h
+++ Source/DoronkoWanko/Public/HJ_ElectricFan.h
@@ -32,10 +32,24 @@ public:
 	UPROPERTY(EditAnywhere)
 	class UStaticMeshComponent* MeshComp;
 
+	UPROPERTY(EditAnywhere)
+	bool IsInRange;
+
 	UFUNCTION(BlueprintCallable)
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	UFUNCTION(BlueprintCallable)
 	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* WingComp;
+
+	UPROPERTY(EditAnywhere)
+	float RotationSpeed;
+
+	UPROPERTY(EditAnywhere)
+	float CurrentRotationAngel;
+
+	UPROPERTY(EditAnywhere)
+	int WingRota = 0;
 };
