f21afb2308056e4166baeb8c433b555256cdeb29
yagae12 <150115300+yagae12@users.noreply.github.com>
2024-07-24 18:17:21 +0900

    palyer

---
+++ Source/DoronkoWanko/Public/GW_Player.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/InputActionValue.h"
+#include "GW_Player.generated.h"
+
+UCLASS()
+class DORONKOWANKO_API AGW_Player : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AGW_Player();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	class UInputMappingContext* IMC_Player;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UInputAction* IA_Move;
+	void OnMyActionMove(const FInputActionValue& Value);
+
+	FVector Direction;
+	
+	UPROPERTY(EditDefaultsOnly)
+	class UInputAction* IA_Look;
+
+	void OnMyActionLook(const FInputActionValue& Value);
+
+	UPROPERTY(EditDefaultsOnly)
+	class UInputAction* IA_Jump;
+
+	void OnMyActionJump(const FInputActionValue& Value);
+
+
+
+};
