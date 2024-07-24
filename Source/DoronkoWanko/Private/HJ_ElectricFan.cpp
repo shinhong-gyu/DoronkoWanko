f04c35452b3c00f4ba04bcd1a72993b8dab9a9ef
Spacialist <a01050254473@gmail.com>
2024-07-24 21:35:51 +0900

    CheckOverlapPoint

--- Source/DoronkoWanko/Private/HJ_ElectricFan.cpp
+++ Source/DoronkoWanko/Private/HJ_ElectricFan.cpp
@@ -2,6 +2,7 @@
 
 
 #include "HJ_ElectricFan.h"
+#include "GW_Player.h"
 #include "Components/BoxComponent.h"
 
 // Sets default values
@@ -10,24 +11,24 @@ AHJ_ElectricFan::AHJ_ElectricFan()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	InputComp = nullptr;
-
 	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
 	SetRootComponent(BoxComp);
 	BoxComp->SetBoxExtent(FVector(50));
 
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
 	MeshComp->SetupAttachment(RootComponent);
-	UE_LOG(LogTemp, Warning, TEXT("E key Presse"));
+
+	BoxComp->SetCollisionProfileName(TEXT("MapObject"));
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &AHJ_ElectricFan::OnOverlapBegin);
+	BoxComp->OnComponentEndOverlap.AddDynamic(this, &AHJ_ElectricFan::OnOverlapEnd);
+
 }
 
 // Called when the game starts or when spawned
 void AHJ_ElectricFan::BeginPlay()
 {
 	Super::BeginPlay();
-	
 
-	// 특정조건이 만족되면 스폰한다 (다른 객체에서 진행) 
 }
 
 // Called every frame
@@ -35,28 +36,32 @@ void AHJ_ElectricFan::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// E키를 누르면 반응을 시작한다
-		APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
-		if (PlayerController)
-		{
-			EnableInput(PlayerController);
-			if (!InputComp)
-			{
-				InputComp = NewObject<UInputComponent>(this);
-				InputComp->RegisterComponent();
-			}
+}
 
-			InputComp->BindAction("Interact", IE_Pressed, this, &AHJ_ElectricFan::Interact);
-		}
+void AHJ_ElectricFan::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	auto* Player = Cast<AGW_Player>(OtherActor);
+
+	if (Player)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("CHECK"));
+	}
+
+}
+
+void AHJ_ElectricFan::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	auto* Player = Cast<AGW_Player>(OtherActor);
+
+	if (Player)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("CHECKOUT"));
+	}
+}
 
 	// 플레이어가 가까이 오면 인식한다 (공통함수) 
 	// 조건을 2가지로 나눈다. A와 B 
-}
-
-void AHJ_ElectricFan::Interact()
-{
-	UE_LOG(LogTemp, Warning, TEXT("E key Presse"));
-}
+	// 
 // A일 때, 선풍기 날개 돌아간다. 
 // A일 때, 투명한 충돌체 소환한다 (선풍기 바람범위) // 충돌체에 따른 물감 이동은 물감 객체에서 구현 
 
--- Source/DoronkoWanko/Public/HJ_ElectricFan.h
+++ Source/DoronkoWanko/Public/HJ_ElectricFan.h
@@ -26,12 +26,16 @@ public:
 	UPROPERTY(EditAnywhere)
 	class UInputComponent* InputComp;
 
-	void Interact();
-
 	UPROPERTY(EditAnywhere)
 	class UBoxComponent* BoxComp;
 
 	UPROPERTY(EditAnywhere)
 	class UStaticMeshComponent* MeshComp;
+
+	UFUNCTION(BlueprintCallable)
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintCallable)
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 };
