0a3291894ac6fb0e2973732c49f1b4dd39ee42e8
shinhong-gyu <shin02198@gmail.com>
2024-08-02 09:16:12 +0900

    8/2 9:16
    Just Commit

---
+++ Source/DoronkoWanko/Private/HJ_FanWing.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HJ_FanWing.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AHJ_FanWing::AHJ_FanWing()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RotationSpeed = 200.0f;
+	CurrentRotationAngel = 0.0f;
+
+	// 困连 巩备 积己 
+	InteractionText = FText::FromString(TEXT("E) INTERACTION"));
+}
+
+// Called when the game starts or when spawned
+void AHJ_FanWing::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHJ_FanWing::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bTurnOn)
+	{
+		CurrentRotationAngel += RotationSpeed * DeltaTime;
+		if (CurrentRotationAngel > 360.0f)
+		{
+			CurrentRotationAngel -= 360.0f;
+		}
+
+		FRotator WingRotation = FRotator(CurrentRotationAngel, 0.0f, 0.0f);
+		BoxComp->SetRelativeRotation(WingRotation);
+	}
+
+
+}
+
+void AHJ_FanWing::InteractionWith()
+{
+	bTurnOn = !bTurnOn;
+}
+
