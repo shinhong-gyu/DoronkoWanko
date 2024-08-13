177927184bfe6aae8145c8a55abaabca9c51f05c
shinhong-gyu <shin02198@gmail.com>
2024-08-13 20:39:07 +0900

    8/13 8:37
    DecalInfo 구조체 추가
    Decal에 물감 튀었을 때 Decal 커지는 것 추가 // 미완

--- Source/DoronkoWanko/Private/HG_Splatter.cpp
+++ Source/DoronkoWanko/Private/HG_Splatter.cpp
@@ -15,6 +15,7 @@
 #include "GameFramework/Actor.h"
 #include "HJ_RoboticVacuum.h"
 #include "Materials/MaterialInstanceDynamic.h"
+#include "DecalInfoStruct.h"
 
 // Sets default values
 AHG_Splatter::AHG_Splatter()
@@ -43,6 +44,8 @@ AHG_Splatter::AHG_Splatter()
 	}
 
 	this->SetLifeSpan(3.0f);
+
+
 }
 
 // Called when the game starts or when spawned
@@ -64,24 +67,6 @@ void AHG_Splatter::Tick(float DeltaTime)
 	UpdataRotation();
 }
 
-void AHG_Splatter::SpawnDecalAtLocation(const FVector& Location, const FRotator& Rotation)
-{
-	if (DecalClass != nullptr) {
-		AHG_DecalActor* Decal = GetWorld()->SpawnActor<AHG_DecalActor>(DecalClass, Location, Rotation);
-		auto* GM = Cast<ADoronkoGameMode>(GetWorld()->GetAuthGameMode());
-		int32 RandNum = FMath::RandRange(1, 9);
-		GM->SetScore(RandNum);
-		GM->UpdataScoreBoard();
-		if (nullptr != Decal) {
-			UDecalComponent* DecalComp = Decal->GetDecal();
-			if (nullptr != DecalComp) {
-				DecalComp->SetWorldLocation(Location);
-				DecalComp->SetWorldRotation(Rotation);
-			}
-		}
-	}
-}
-
 void AHG_Splatter::OnMyBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if (OtherActor->IsA<ARoboticVacuum>()) {
@@ -89,43 +74,55 @@ void AHG_Splatter::OnMyBeginOverlap(UPrimitiveComponent* OverlappedComponent, AA
 	}
 	FVector SpawnLocation = GetActorLocation();
 	FRotator SpawnRoation;
-	this->Destroy();
+	FDecalInfo SpawnDeaclInfo, HittedDecalInfo = FDecalInfo();
 	float RandNum = FMath::FRandRange(100.0f, 150.0f);
 	auto* GM = Cast<ADoronkoGameMode>(GetWorld()->GetAuthGameMode());
 	int32 RanInt = FMath::RandRange(1, 9);
-	GM->SetScore(RanInt);
-	GM->UpdataScoreBoard();
 	FVector end = SpawnLocation + Velocity.GetSafeNormal() * 10000;
+
 	FHitResult hitInfo;
 	FCollisionQueryParams params;
 	FCollisionObjectQueryParams QParams;
+
+
+	this->Destroy();
+	GM->SetScore(RanInt);
+	GM->UpdataScoreBoard();
+
 	QParams.AddObjectTypesToQuery(ECC_WorldDynamic);
 	QParams.AddObjectTypesToQuery(ECC_PhysicsBody);
 	params.AddIgnoredActor(this);
+
 	bool bHit = GetWorld()->LineTraceSingleByObjectType(hitInfo, SpawnLocation, end, QParams, params);
 	if (bHit)
 	{
-		UDecalComponent* Decal = UGameplayStatics::SpawnDecalAttached(SelectedMaterial, FVector(-5.0f, RandNum, RandNum), OtherComp, NAME_None, hitInfo.ImpactPoint, hitInfo.ImpactNormal.ToOrientationRotator(), EAttachLocation::KeepWorldPosition);
-		//GetWorld()->SpawnActor<AActor>(NormalArrow, hitInfo.ImpactPoint, hitInfo.ImpactNormal.ToOrientationRotator());
-		//DrawDebugLine(GetWorld(), hitInfo.ImpactPoint, (hitInfo.ImpactNormal * 10000.0f), FColor::Blue, false, 10000.0f);
-		//GetWorld()->SpawnActor<AActor>(NormalArrow, hitInfo.ImpactPoint, hitInfo.ImpactNormal.ToOrientationRotator());
-
-		if (Decal) {
-			UMaterialInstanceDynamic* DynamicMaterial = UMaterialInstanceDynamic::Create(Decal->GetDecalMaterial(), this);
-			if (DynamicMaterial) {
-				DynamicMaterial->SetVectorParameterValue("Color", MyColor);
-				Decal->SetDecalMaterial(DynamicMaterial);
+		HittedDecalInfo = IsDecalInRange(hitInfo.ImpactPoint, RandNum, RandNum);
+		if (HittedDecalInfo.DecalComp != nullptr && MyColor == HittedDecalInfo.Color) {
+			UE_LOG(LogTemp,Warning,TEXT("aAAAAAAAAAAAAAAAAA"));
+			HittedDecalInfo.DecalComp->DecalSize += FVector(0.0f, 1000.0f, 1000.0f);
+		}
+		else {
+			UDecalComponent* Decal = UGameplayStatics::SpawnDecalAttached(SelectedMaterial, FVector(-5.0f, RandNum, RandNum), OtherComp, NAME_None, hitInfo.ImpactPoint, hitInfo.ImpactNormal.ToOrientationRotator(), EAttachLocation::KeepWorldPosition);
+			if (Decal) {
+				UMaterialInstanceDynamic* DynamicMaterial = UMaterialInstanceDynamic::Create(Decal->GetDecalMaterial(), this);
+				if (DynamicMaterial) {
+					DynamicMaterial->SetVectorParameterValue("Color", MyColor);
+					Decal->SetDecalMaterial(DynamicMaterial);
+				}
 			}
+			SpawnDeaclInfo.DecalComp = Decal;
+			SpawnDeaclInfo.Color = MyColor;
+			SpawnDeaclInfo.Pos = hitInfo.ImpactPoint;
+			GM->SpawnedDecalArr.Add(SpawnDeaclInfo);
 		}
 	}
-	TArray<AHG_MissonStamp*> StampArray = IsStampInRange(hitInfo.ImpactPoint,RandNum,RandNum);
+	TArray<AHG_MissonStamp*> StampArray = IsStampInRange(hitInfo.ImpactPoint, RandNum, RandNum);
 	if (StampArray.Num() != 0) {
 		for (auto s : StampArray) {
 			s->Decal->SetVisibility(true);
 			GM->StampCount++;
 		}
 	}
-
 }
 
 
@@ -154,7 +151,7 @@ TArray<AHG_MissonStamp*> AHG_Splatter::IsStampInRange(FVector Pos, float Param1,
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), StampFactory, StampArray);
 	for (auto a : StampArray) {
 		FVector Dist = Pos - (a->GetActorLocation());
-		if ((Param1/2) * (Param1/2) + (Param2/2) * (Param2/2) > Dist.Size() * Dist.Size()) {
+		if ((Param1 / 2) * (Param1 / 2) + (Param2 / 2) * (Param2 / 2) > Dist.Size() * Dist.Size()) {
 			Result.Add(Cast<AHG_MissonStamp>(a));
 		}
 	}
@@ -164,5 +161,29 @@ TArray<AHG_MissonStamp*> AHG_Splatter::IsStampInRange(FVector Pos, float Param1,
 void AHG_Splatter::SetMyColor(FLinearColor Value)
 {
 	MyColor = Value;
+}
+
+FDecalInfo AHG_Splatter::IsDecalInRange(FVector Pos, float Param1, float Param2)
+{
+	FVector Dist;
+	FVector Min = FVector(1000.0f, 1000.0f, 1000.0f);
+	int32 RetIdx = -1;
+	ADoronkoGameMode* GM = Cast<ADoronkoGameMode>(GetWorld()->GetAuthGameMode());
+	for (int i = 0; i < GM->SpawnedDecalArr.Num(); i++) {
+		Dist = Pos - GM->SpawnedDecalArr[i].DecalComp->GetComponentLocation();
+		// 만약 원하는 거리 안에 있으면
+		if ((Param1 / 2) * (Param1 / 2) + (Param2 / 2) * (Param2 / 2) > Dist.Size() * Dist.Size()) {
+			// 가장 가까운 DecalComp찾기
+			// Min 값 업데이트
+			if (Min.Size() >= Dist.Size()) {
+				Min = Dist;
+				RetIdx = i;
+			}
+		}
+	}
+	if (RetIdx > 0 && RetIdx < GM->SpawnedDecalArr.Num())
+		return GM->SpawnedDecalArr[RetIdx];
+	else
+		return FDecalInfo();
 }
 
