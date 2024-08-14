c7de237933371fd1000a3009cc650efcd8548321
Spacialist <a01050254473@gmail.com>
2024-08-14 14:55:35 +0900

    Editing MiniMap

---
+++ Source/DoronkoWanko/Public/HJ_MinimapUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HJ_MinimapUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DORONKOWANKO_API UHJ_MinimapUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
